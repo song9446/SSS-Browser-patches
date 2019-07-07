5759252cb63a7fa26c9434426360e3d0ff020eb4
Eunchul Song <song9446@unist.ac.kr>
2019-07-07 15:27:21 +0900

    branding modified

--- build.bash
+++ build.bash
@@ -1,6 +1,31 @@
-ninja -C out/Default chrome_modern_public_apk
+autoninja -C out/Default chrome_public_apk
 
-mv out/Default/apks/ChromeModernPublic.apk out/Default/apks/Dodgeman.apk
+mv out/Default/apks/ChromePublic.apk out/Default/apks/DodgeChrome.apk
 
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ${BASH_SOURCE%/*}/keystores/eunchul.jks out/Default/apks/Dodgeman.apk dodgeman-browser
-third_party/android_tools/sdk/build-tools/27.0.3/zipalign -v -p 4 out/Default/apks/Dodgeman.apk out/Default/apks/Dodgeman_aligned.apk
+zip -d out/Default/apks/DodgeChrome.apk META-INF/\*
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ${BASH_SOURCE%/*}/keystores/eunchul.jks out/Default/apks/DodgeChrome.apk dodgechrome
+third_party/android_tools/sdk/build-tools/27.0.3/zipalign -v -p 4 out/Default/apks/DodgeChrome.apk out/Default/apks/DodgeChrome_aligned.apk
+
+
+autoninja -C out/Default chrome_modern_public_apk
+
+mv out/Default/apks/ChromeModernPublic.apk out/Default/apks/DodgeModernChrome.apk
+
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ${BASH_SOURCE%/*}/keystores/eunchul.jks out/Default/apks/DodgeModernChrome.apk dodgechrome
+third_party/android_tools/sdk/build-tools/27.0.3/zipalign -v -p 4 out/Default/apks/DodgeModernChrome.apk out/Default/apks/DodgeModernChrome_aligned.apk
+
+
+autoninja -C out/Default monochrome_public_apk
+
+mv out/Default/apks/MonoChromePublic.apk out/Default/apks/DodgeMonoChrome.apk
+
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ${BASH_SOURCE%/*}/keystores/eunchul.jks out/Default/apks/DodgeMonoChrome.apk dodgechrome
+third_party/android_tools/sdk/build-tools/27.0.3/zipalign -v -p 4 out/Default/apks/DodgeMonoChrome.apk out/Default/apks/DodgeMonoChrome_aligned.apk
+
+
+autoninja -C out/Default trichrome_public_apk
+
+mv out/Default/apks/TriChromePublic.apk out/Default/apks/DodgeTriChrome.apk
+
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ${BASH_SOURCE%/*}/keystores/eunchul.jks out/Default/apks/DodgeTriChrome.apk dodgechrome
+third_party/android_tools/sdk/build-tools/27.0.3/zipalign -v -p 4 out/Default/apks/DodgeTriChrome.apk out/Default/apks/DodgeTriChrome_aligned.apk
